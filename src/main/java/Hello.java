d56ac7d68e75783a6f8dcfe76e19270488620e6b
Phill Conrad <pconrad@cs.ucsb.edu>
2020-10-05 13:46:24 -0700

    Update Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        // Replace this comment with code to print "Hello, World!"
+        System.out.println("This is the wrong output!");
     }
 }
